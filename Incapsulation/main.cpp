41f4e5d86f5b52ba1adf235c6d51a62214db68a3
xrlogic1 <xrlogic1@gmail.com>
2023-07-12 21:40:52 +0400

    *

--- Incapsulation/main.cpp
+++ Incapsulation/main.cpp
@@ -1,21 +1,47 @@
 #include "Array.h"
 #include <list>
+#include <algorithm>
+#include <iostream>
 
-class Repo
+
+template<typename T> class Message
 {
-private:
-    std::vector<int> m_data;
-
-    Repo() : m_data(20)
+public:
+    Message()
     {
-
-    }
-
-    Repo() : m_data({10, 20, 30})
-    {
-
+        std::cout << "Generic spec" << '\n';
     }
 };
+
+template<> class Message<int>
+{
+public:
+    Message()
+    {
+        std::cout << "Full spec" << '\n';
+    }
+};
+
+//class Repo
+//{
+//private:
+//    std::vector<int> m_data;
+//
+//    Repo() : m_data(20)
+//    {
+//
+//    }
+//
+//    Repo() : m_data({10, 20, 30})
+//    {
+//        auto result = std::find(m_data.begin(), m_data.end(), 20);
+//
+//        if (result != m_data.end())
+//        {
+//            int value = *result;
+//        }
+//    }
+//};
 
 
 void print(const Array<int>& arr)
@@ -28,7 +54,9 @@ void print(const Array<int>& arr)
 
 int main()
 {
-    Array<int> array;
+    Message<int> msg;
+
+    /*Array<int> array;
 
     array.append(13);
     array.append(12);
@@ -41,5 +69,5 @@ int main()
 
     array.sort();
 
-    print(array);
+    print(array);*/
 }
